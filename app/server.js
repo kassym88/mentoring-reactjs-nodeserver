6cbcd98fa4f7bfb48e113784b2e7cc8ca74ede2d
kassym88 <kassym88@gmail.com>
2019-03-11 11:30:45 +0600

    refactor

--- app/server.js
+++ app/server.js
@@ -1,19 +1,7 @@
 const
-    http = require('http'),
-    // https = require('https'),
+    port = 80,
     app = require('./app');
 
-const httpServer = http.createServer(app);
-httpServer.listen(80, () => {
-    const host = httpServer.address().address;
-    const port = httpServer.address().port;
-    console.log(`Server listening at http://${host === '::' ? 'localhost' : host}:${port}`);
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
 });
-
-// SSL Server
-// const httpsServer = https.createServer({}, app);
-// httpsServer.listen(443, () => {
-//     const host = httpsServer.address().address;
-//     const port = httpsServer.address().port;
-//     console.log(`SSL server listening at http://${host === '::' ? 'localhost' : host}:${port}`);
-// });
